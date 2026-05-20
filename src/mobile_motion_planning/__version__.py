7004d8095598ad3e66d661779fd58ae6f1054695
daricht <80166694+daricht@users.noreply.github.com>
2026-05-20 17:19:23 +0200

    added version.py back to maintain pip installability

---
+++ src/mobile_motion_planning/__version__.py
@@ -0,0 +1,19 @@
+__title__ = 'mobile_motion_planning'
+__description__ = 'Trajectory generation and rolling replanning for a UR manipulator on a moving base'
+__url__ = 'https://github.com/augmentedfabricationlab/mobile_motion_planning'
+__version__ = '0.1.0'
+__author__ = 'Augmented Fabrication Lab'
+__author_email__ = 'david.richter@tum.de'
+__license__ = 'MIT license'
+__copyright__ = 'Copyright 2026 Augmented Fabrication Lab'
+
+__all__ = [
+    '__author__',
+    '__author_email__',
+    '__copyright__',
+    '__description__',
+    '__license__',
+    '__title__',
+    '__url__',
+    '__version__',
+]
